a545f32a2a61814ea43bcd29a9da9c77b873c6e0
d.naoto <d.naoto>
2022-11-09 01:32:01 +0900

    add license plate

---
+++ src/data/license-plate.js
@@ -0,0 +1,54 @@
+const questions = [
+  {
+    questionTxt: "ヨーロッパで前と後ろのナンバープレートが黄色の国はルクセンブルクとどこ？",
+    questionImg: "",
+    incorrectAnswers: [
+      "ドイツ",
+      "イタリア",
+      "スイス",
+    ],
+    correctAnswer: "オランダ",
+  },
+  {
+    questionTxt: "ナンバープレートで前が白色、後ろが黄色でない国は？",
+    questionImg: "",
+    incorrectAnswers: [
+      "イギリス",
+      "キプロス",
+      "ジブラルタル",
+    ],
+    correctAnswer: "イタリア",
+  },
+  {
+    questionTxt: "ナンバープレートの左右に青い線が入っていない国は？",
+    questionImg: "",
+    incorrectAnswers: [
+      "イタリア",
+      "アルバニア",
+      "フランス",
+    ],
+    correctAnswer: "ポルトガル",
+  },
+  {
+    questionTxt: "",
+    questionImg: "license-plates/portugal.png",
+    incorrectAnswers: [
+      "イタリア",
+      "スペイン",
+      "フランス",
+    ],
+    correctAnswer: "ポルトガル",
+  },
+  {
+    questionTxt: "",
+    questionImg: "license-plates/bhutan.png",
+    incorrectAnswers: [
+      "バングラデシュ",
+      "ミャンマー",
+      "ラオス",
+    ],
+    correctAnswer: "ブータン",
+  }
+];
+
+export default questions;
